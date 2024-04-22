5a09b3223d22f5d9f6b87fd2b76abc13c789889b
Simon <simon.ponitzki@gmail.com>
2024-04-22 10:03:48 +0200

    test auth

--- CommurideTests/AuthTests.cs
+++ CommurideTests/AuthTests.cs
@@ -1,15 +1,30 @@
 using CommurideModels.DTOs.AppUser;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Testing;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using System.Net;
 using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
 using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
 
 namespace CommurideTests
 {
-    public class AuthTests(CommurideWebApplicationFactory<Program> factory) : IClassFixture<CommurideWebApplicationFactory<Program>>
+    public class AuthTests(CommurideWebApplicationFactory<Program> factory, ITestOutputHelper output) : IClassFixture<CommurideWebApplicationFactory<Program>>
     {
         private readonly HttpClient _client = factory.CreateClient();
-        DateTime now = DateTime.UtcNow;
+        private readonly ITestOutputHelper _output = output;
+
+        // Use of TheoryData for strongly typed data tests
+        public static TheoryData<CreateAppUserDTO> data = new TheoryData<CreateAppUserDTO>() {
+           /* new CreateAppUserDTO() { Username = "test_user", Password = "pass", Password2 = "pass", BirthDate = new DateTime() },
+            new CreateAppUserDTO() { Username = "test_user2", Password = "pass", Password2 = "pass", BirthDate = new DateTime() },
+            new CreateAppUserDTO() { Username = "test_user3", Password = "pass", Password2 = "pass", BirthDate = new DateTime() },*/
+            new CreateAppUserDTO() { Username = "test_user4", Password = "pass", Password2 = "pass", BirthDate = new DateTime() }
+        };
 
         [Fact]
         public async Task TestMethod1()
@@ -18,12 +33,18 @@ namespace CommurideTests
             Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
         }
 
-       /* [Theory]
-        [InlineData(new CreateAppUserDTO() { BirthDate = DateTime.MinValue, Password = "pass", Password2 = "pass", Username = "test_user" })]
-        public async Task Register(CreateAppUserDTO userDTO)
+        [Theory]
+        [MemberData(nameof(data))]
+        public async Task Register(CreateAppUserDTO appUserDTO)
         {
-            
-
-        }*/
+            string stringContent = JsonSerializer.Serialize(appUserDTO);
+            var content = new StringContent(stringContent, encoding: Encoding.UTF8, mediaType: "application/json");
+            _output.WriteLine(await content.ReadAsStringAsync());
+            var response = await _client.PostAsync("api/Auth/Register", content);
+            await Console.Out.WriteLineAsync(await response.Content.ReadAsStringAsync());
+            _output.WriteLine(await response.Content.ReadAsStringAsync());
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+        
     }
 }
