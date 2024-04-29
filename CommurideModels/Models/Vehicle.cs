e9ea18b143150bfd4f5c312d7f632f55fd1d5197
Epouvaantail <coutouzis.kilian@gmail.com>
2024-04-29 15:15:27 +0200

    update

--- CommurideModels/Models/Vehicle.cs
+++ CommurideModels/Models/Vehicle.cs
@@ -1,5 +1,6 @@
 
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using CommurideModels.DTOs.Vehicle;
 using CommurideModels.Models;
 
@@ -56,7 +57,9 @@ namespace Models
         public StatusVehicle Status{ get; set; }
         [Required]
         public int NbPlaces { get; set; }
+        [ForeignKey("UserId")]
         public AppUser? User { get; set; }
+        [ForeignKey("RentId")]
         public ICollection<Rent>? Rents { get; } = new List<Rent>();
 
     }
