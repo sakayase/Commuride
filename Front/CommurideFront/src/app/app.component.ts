6bb038e7266e99ebfe90b7497f4b75f2ee14739a
Epouvaantail <coutouzis.kilian@gmail.com>
2024-06-12 11:00:15 +0200

    update

--- Front/CommurideFront/src/app/app.component.ts
+++ Front/CommurideFront/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthPageComponent } from './pages/auth-page/auth-page.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AuthPageComponent],
+  imports: [ CommonModule, RouterOutlet, AuthPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- Front/CommurideFront/src/app/components/skeleton/skeleton.component.ts
+++ Front/CommurideFront/src/app/components/skeleton/skeleton.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-skeleton',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './skeleton.component.html',
   styleUrl: './skeleton.component.scss'
 })
 export class SkeletonComponent {
-  title = 'skeleton';
+  isMobile: boolean = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      const userAgent = window.navigator.userAgent;
+      this.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+    }
+  }
 }
